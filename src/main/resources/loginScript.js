66d19752221dc3ae8cdb7a1ecff4a2473c56f0bb
wlcross <william589@revature.net>
2022-06-27 16:59:17 -0400

    Simple Manager Functionality

--- src/main/resources/loginScript.js
+++ src/main/resources/loginScript.js
@@ -37,15 +37,61 @@ async function login() {
         .catch((error) => {
             console.log(error);
         });
+}
 
+async function managerLogin() {
+    console.log("login button pressed")
 
+    // gather input from the user - using our DOM
+    let uname = document.getElementById('uname').value;
+
+    let pass = document.getElementById('pass').value;
+
+    // print that to the console
+    console.log(uname);
     
+    // send a GET request to our backend using the Fetch API
+    // fetch method returns a Promise
+    let res = await fetch(
+                        `${baseUrl}/users/${uname}`
+                    );
+    let resJson = await res.json()
+        // .then will execute if the promise is successfully resolved
+        // .then() takes a function as an argument
+        .then((resp) => {
+            
+            console.log(resp); // this is where we will eventually put our DOM manipulation if needed
+
+            if (!resp.manager) {
+                console.log("Not a manager");
+                lackPerm();
+            } else if (resp.pass == pass) {
+                sessionStorage.setItem("id", resp.id);
+                window.location.assign("managerHomePage.html");
+            } else {
+                console.log("Wrong Password");
+                wrongPass();
+            }
+
+        })
+        // .catch will execute if there's any error
+        .catch((error) => {
+            console.log(error);
+        });
 }
 
 function wrongPass() {
     let slot = document.getElementById("error");
     let text = document.createElement("p");
-    let tempText = "INCORRECT LOG-IN INFORMATION";
+    let tempText = "INCORRECT PASSWORD";
+    text.innerHTML = tempText;
+    slot.innerHTML = "";
+    slot.appendChild(text);
+}
+function lackPerm() {
+    let slot = document.getElementById("error");
+    let text = document.createElement("p");
+    let tempText = "THIS USER IS NOT A MANAGER";
     text.innerHTML = tempText;
     slot.innerHTML = "";
     slot.appendChild(text);
---
+++ src/main/resources/managerHome.js
@@ -0,0 +1,5 @@
+let baseUrl = "http://localhost:8080"; // /users, /users/1
+
+async function viewRequests() {
+    window.location.assign("managerViewRequestList.html");
+}
